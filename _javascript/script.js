b41733ae1a623c472be03d3db5772c6f38dbd50f
Thiago Cruz <thagocruz@gmail.com>
2018-02-16 18:54:27 -0200

    Delete script.js

--- _javascript/script.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("listaTarefas", []);
-app.controller("Controller", function($scope){
-    $scope.pendentes = "Lista de pendentes";
-    $scope.concluidas = "Lista de concluidas";
-    $scope.tarefas = listaDeTarefas
-});
