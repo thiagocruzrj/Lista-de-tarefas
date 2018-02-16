23abf87eca4f9799136ac41829b81aac23a4dab1
Thiago Cruz <thagocruz@gmail.com>
2018-02-16 18:54:42 -0200

    Delete scriptCategorias.js

--- _javascript/scriptCategorias.js
+++
@@ -1,8 +0,0 @@
-angular.module("listaDeCategorias", []);
-angular.module("listaDeCategorias").controller("listaDeCategoriasCtrl", function($scope){
-    $scope.categorias = [
-        {nome: 'Casa'},
-        {nome: 'Trabalho'},
-        {nome: 'Faculdade'}
-    ];
-});
